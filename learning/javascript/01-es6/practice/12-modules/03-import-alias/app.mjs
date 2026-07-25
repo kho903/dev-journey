4cbc739acdb90e531720485b9aa3d93c91f223db
kho903 <gmldnr2222@naver.com>
2026-07-25 19:52:53 +0900

    feat: add import alias practice
    Refs #15

---
+++ learning/javascript/01-es6/practice/12-modules/03-import-alias/app.mjs
@@ -0,0 +1,14 @@
+import {
+  name as userName,
+  role as userRole,
+  introduce as introduceUser,
+} from "./user.mjs";
+
+console.log("userName :", userName);
+console.log("userRole :", userRole);
+console.log("introduceUser() :", introduceUser());
+/*
+userName : JIHUN
+userRole : backend
+introduceUser() : JIHUN is studying backend development.
+*/
---
+++ learning/javascript/01-es6/practice/12-modules/03-import-alias/user.mjs
@@ -0,0 +1,8 @@
+const name = "JIHUN";
+const role = "backend";
+
+function introduce() {
+  return `${name} is studying ${role} development.`;
+}
+
+export { name, role, introduce };
