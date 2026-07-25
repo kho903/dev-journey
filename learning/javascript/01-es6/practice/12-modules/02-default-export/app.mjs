fe373c259efce29616d2fb3e7fe818d69267de8d
kho903 <gmldnr2222@naver.com>
2026-07-25 19:48:44 +0900

    feat: add default export practice
    Refs #15

---
+++ learning/javascript/01-es6/practice/12-modules/02-default-export/app.mjs
@@ -0,0 +1,12 @@
+import member from "./user.mjs";
+
+console.log("member :", member);
+console.log("member.name :", member.name);
+console.log("member.age :", member.age);
+console.log("member.role :", member.role);
+/*
+member : { name: 'JIHUN', age: 29, role: 'backend' }
+member.name : JIHUN
+member.age : 29
+member.role : backend
+*/
