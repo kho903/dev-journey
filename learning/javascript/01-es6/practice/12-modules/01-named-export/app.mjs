4102cea560e734cfad6fe54be2b583c2d6fcef70
kho903 <gmldnr2222@naver.com>
2026-07-25 19:45:27 +0900

    feat: add named export practice
    Refs #15

---
+++ learning/javascript/01-es6/practice/12-modules/01-named-export/app.mjs
@@ -0,0 +1,12 @@
+import { name, age, role, introduce } from "./user.mjs";
+
+console.log("name :", name);
+console.log("age :", age);
+console.log("role :", role);
+console.log("introduce() :", introduce);
+/*
+name : JIHUN
+age : 29
+role : backend
+introduce() : [Function: introduce]
+*/
---
+++ learning/javascript/01-es6/practice/12-modules/01-named-export/user.mjs
@@ -0,0 +1,9 @@
+const name = "JIHUN";
+const age = 29;
+const role = "backend";
+
+function introduce() {
+  return `My name is ${name}, and I am studying ${role} development.`;
+}
+
+export { name, age, role, introduce };
