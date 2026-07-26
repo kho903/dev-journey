1b22a890c708bbbad3bbf84f22453cda41d22ea0
kho903 <gmldnr2222@naver.com>
2026-07-26 14:31:53 +0900

    feat: add named and default export practice
    Refs #15

---
+++ learning/javascript/01-es6/practice/12-modules/08-named-and-default-export/app.mjs
@@ -0,0 +1,13 @@
+import multiply, { PI, add, subtract } from "./calculator.mjs";
+
+console.log("PI :", PI);
+console.log("add(10, 20) :", add(10, 20));
+console.log("subtract(20, 10) :", subtract(20, 10));
+console.log("multiply(10, 20) :", multiply(10, 20));
+
+/*
+PI : 3.141592
+add(10, 20) : 30
+subtract(20, 10) : 10
+multiply(10, 20) : 200
+*/
---
+++ learning/javascript/01-es6/practice/12-modules/08-named-and-default-export/calculator.mjs
@@ -0,0 +1,16 @@
+const PI = 3.141592;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+export { PI, add, subtract };
+export default multiply;
