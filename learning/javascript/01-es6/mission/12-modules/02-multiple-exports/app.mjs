d2441720f8441740e9f4f37aa8807d424183d64e
kho903 <gmldnr2222@naver.com>
2026-07-26 15:16:40 +0900

    feat: solve multiple exports mission
    Refs #15

---
+++ learning/javascript/01-es6/mission/12-modules/02-multiple-exports/app.mjs
@@ -0,0 +1,10 @@
+import { name, age, role } from "./user.mjs";
+
+console.log("name :", name);
+console.log("age :", age);
+console.log("role :", role);
+/*
+name : JIHUN
+age : 29
+role : backend
+*/
---
+++ learning/javascript/01-es6/mission/12-modules/02-multiple-exports/user.mjs
@@ -0,0 +1,5 @@
+const name = "JIHUN";
+const age = 29;
+const role = "backend";
+
+export { name, age, role };
