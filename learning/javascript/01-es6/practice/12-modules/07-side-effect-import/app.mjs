de7b98b98299c358d70f0d8d24f78f8865f949c1
kho903 <gmldnr2222@naver.com>
2026-07-26 14:29:42 +0900

    feat: add side-effect import practice
    Refs #15

---
+++ learning/javascript/01-es6/practice/12-modules/07-side-effect-import/app.mjs
@@ -0,0 +1,9 @@
+import "./setup.mjs";
+
+console.log("Application is running");
+/*
+Application setup started
+Environment: development
+Application setup completed
+Application is running
+*/
---
+++ learning/javascript/01-es6/practice/12-modules/07-side-effect-import/setup.mjs
@@ -0,0 +1,6 @@
+console.log("Application setup started");
+
+const environment = "development";
+
+console.log(`Environment: ${environment}`);
+console.log(`Application setup completed`);
