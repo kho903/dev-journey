234510aad532a1f0de0ff7fb6ffc736cfca71d67
kho903 <gmldnr2222@naver.com>
2026-07-26 15:52:32 +0900

    feat: solve named and default export mission
    Refs #15

---
+++ learning/javascript/01-es6/mission/12-modules/08-named-default-export/app.mjs
@@ -0,0 +1,12 @@
+import multiply, { PI, add, subtract } from "./calculator.mjs";
+
+console.log("PI :", PI);
+console.log("add :", add(10, 20));
+console.log("subtract :", subtract(20, 10));
+console.log("multiply :", multiply(10, 20));
+/*
+PI : 3.141592
+add : 30
+subtract : 10
+multiply : 200
+*/
---
+++ learning/javascript/01-es6/mission/12-modules/08-named-default-export/calculator.mjs
@@ -0,0 +1,13 @@
+export const PI = 3.141592;
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
+
+export default function multiply(a, b) {
+  return a * b;
+}
