58f83bb46b93ee3d14923c71a3d740ed9036e33c
kho903 <gmldnr2222@naver.com>
2026-07-26 15:32:19 +0900

    feat: solve default export mission
    Refs #15

---
+++ learning/javascript/01-es6/mission/12-modules/04-default-export/app.mjs
@@ -0,0 +1,4 @@
+import member from "./user.mjs";
+
+console.log(member);
+// { name: 'JIHUN', age: 29, role: 'backend' }
---
+++ learning/javascript/01-es6/mission/12-modules/04-default-export/user.mjs
@@ -0,0 +1,7 @@
+const user = {
+  name: "JIHUN",
+  age: 29,
+  role: "backend",
+};
+
+export default user;
