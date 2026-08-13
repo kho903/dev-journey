0b891d3acab07e626f82cfc30290a2bd5413e9e3
kho903 <gmldnr2222@naver.com>
2026-08-13 22:02:09 +0900

    feat: implement jsx and component practice
    Refs #22

---
+++ learning/react/01-fundamentals/practice/react-fundamentals-practice/src/practice/01-jsx-component/Practice.jsx
@@ -0,0 +1,11 @@
+function Profile() {
+  return (
+    <section className="profile">
+      <h1>JIHUN</h1>
+      <p>Backend Developer</p>
+      <p>React</p>
+    </section>
+  );
+}
+
+export default Profile;
