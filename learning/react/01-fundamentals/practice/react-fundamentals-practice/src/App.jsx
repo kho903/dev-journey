64bbbad6df9c38db991228c0294c0ebaea41e779
kho903 <gmldnr2222@naver.com>
2026-08-14 09:08:40 +0900

    feat: implement props practice
    Refs #22

--- learning/react/01-fundamentals/practice/react-fundamentals-practice/src/App.jsx
+++ learning/react/01-fundamentals/practice/react-fundamentals-practice/src/App.jsx
@@ -1,7 +1,13 @@
 import Profile from "./practice/01-jsx-component/Practice.jsx";
+import PropsPractice from "./practice/02-props/Practice.jsx";
 
 function App() {
-  return <Profile />;
+  return (
+    <section>
+      <Profile />
+      <PropsPractice />
+    </section>
+  );
 }
 
 export default App;
