63c9ecf62b1724d2929257fe7c5135416620dfc3
kho903 <gmldnr2222@naver.com>
2026-08-15 18:32:35 +0900

    feat: implement array rendering practice
    Refs #22

--- learning/react/01-fundamentals/practice/react-fundamentals-practice/src/App.jsx
+++ learning/react/01-fundamentals/practice/react-fundamentals-practice/src/App.jsx
@@ -1,6 +1,7 @@
 import Profile from "./practice/01-jsx-component/Practice.jsx";
 import PropsPractice from "./practice/02-props/Practice.jsx";
 import ChildrenPractice from "./practice/03-children-composition/Practice.jsx";
+import ArrayRenderingPractice from "./practice/04-array-rendering/Practice.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Profile />
       <PropsPractice />
       <ChildrenPractice />
+      <ArrayRenderingPractice />
     </section>
   );
 }
